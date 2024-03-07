a503a4321205f1e2d731e6411ef430b97afa96c8
diangou.cmr09@gmail.com <122739972+Diangou@users.noreply.github.com>
2024-03-07 10:35:05 +0100

    06/03/2024

--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -9,60 +9,86 @@ public class EnemyPatrol : MonoBehaviour
     public BoxCollider2D bc;
     public float distanceDetection = 0.5f;
     public LayerMask obstaclesLayer;
-    public bool isFacingRight = true ;
 
+    public bool isFacingRight = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    private void FixUpdate(){
-        rb.velocity = new Vector2 (
+
+    private void Update()
+    {
+        isFacingRight = transform.right.normalized.x > 0;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(
             speed * transform.right.normalized.x,
             rb.velocity.y
         );
 
-        if(HasCollisionWithObstacle() ) {
+        if (HasCollisionWithObstacle())
+        {
             Flip();
         }
-
     }
 
-public void Flip() {
-            transform.Rotate(0, 180, 0);
-        }
-    
-    public bool HasCollisionWithObstacle () {
-        RaycastHit2D hit =Physics2D.Linecast(
-            bc.bounds.center,
-            bc.bounds.center + new Vector3(distanceDetection,0,0),
+    public void Flip()
+    {
+        transform.Rotate(0, 180, 0);
+    }
+
+    public bool HasNotTouchedGround()
+    {
+        Vector2 centerPosition = new Vector2(
+            bc.bounds.center.x + (transform.right.normalized.x * (bc.bounds.size.x / 2)),
+            bc.bounds.min.y
+        );
+        Collider2D hit = Physics2D.OverlapCircle(
+            centerPosition,
+            0.1f,
             obstaclesLayer
         );
-        return hit.collider != null;
+        return hit.transform == null;
     }
 
-    private void OnDrawGizmos () {
-         if (bc != null)
-         {
-            Gizmos.color = Color.cyan;;
+    public bool HasCollisionWithObstacle()
+    {
+        RaycastHit2D hit = Physics2D.Linecast(
+            bc.bounds.center,
+            bc.bounds.center + new Vector3(distanceDetection * transform.right.normalized.x, 0, 0),
+            obstaclesLayer
+        );
+
+        if(hit) {
+            return false;
+        }
+        return true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (bc != null)
+        {
+            // transform.right.normalized.x
+            Gizmos.color = Color.cyan;
             Gizmos.DrawLine(
                 bc.bounds.center,
-                bc.bounds.center + new Vector3(distanceDetection * transform.right.normalized.x,0,0)
-
-
+                bc.bounds.center + new Vector3(distanceDetection * transform.right.normalized.x, 0, 0)
             );
 
             Gizmos.color = Color.yellow;
-            Gizmos.DrawSphere (
+            Gizmos.DrawSphere(
                 new Vector2(
                     bc.bounds.center.x + (transform.right.normalized.x * (bc.bounds.size.x / 2)),
                     bc.bounds.min.y
                 ),
                 0.1f
             );
-         }
+        }
     }
 
 }
-
